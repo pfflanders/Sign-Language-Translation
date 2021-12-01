fbf1b56f443a08263bf919f64dbe27aada730c46
mansakrishna23 <mansakrishna23@gmail.com>
2021-12-01 00:45:07 -0800

    updated webapp

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, g, render_template, request
 from PIL import Image
 
-import cv2 as cv
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import InputLayer, Flatten, Dense, ReLU, Softmax, Conv2D, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import CategoricalAccuracy
 
 import sklearn as sk
 import matplotlib.pyplot as plt
@@ -26,17 +33,44 @@ def submit():
         return render_template("submit.html")
     else:
         try:
+            alphabet = list("abcdefghiklmnopqrstuvwxy") + ['blank']
+
             # Retrieve the image of hand gesture
             img = Image.open(request.files['image'].stream)
             img = np.asarray(img)
 
-            # reshape into appropriate format for prediction
-            # Image resolution = (56 x 56)
-            # x = img.reshape(1, 3136)
+            # Alter the shape of array to get asl_guess
+            img = img/255.0
+            x = img[:,:].reshape((1,56,56,1))
+            x = (x - x.min())/(x.max() - x.min())
 
             # load pre-trained model and get prediction
-            # Reference to model included here
-            sign = "A"
+            IMG_SIZE = (56,56)
+            IMG_SHAPE = IMG_SIZE + (1,)
+            base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE, include_top=False, weights=None)
+
+            model = Sequential([
+                base_model,
+                Flatten(),
+                Dense(25),
+                Softmax(),
+            ])
+            model.compile(
+                optimizer="adam",
+                loss=CategoricalCrossentropy(),
+                metrics=[CategoricalAccuracy()]
+            )
+            model.load_weights('model/variables/variables')
+
+            asl_guess = model.predict(x)
+            accuracy = np.round(np.max(asl_guess)*100, 2)
+            character = np.argmax(asl_guess)
+            sign = alphabet[character]
+
+            # sign = "A"
+
+            # loaded_model = pickle.load(open("model/saved_model.pb", 'rb'))
+            # sign = loaded_model.predict(x)[0]
 
             # plot the image itself
             fig = Figure(figsize = (3, 3))
@@ -53,6 +87,6 @@ def submit():
             pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
 
             # Render the template with the prediction and image
-            return render_template('submit.html', letter=sign, image=pngImageB64String)
+            return render_template('submit.html', letter=sign, acc=accuracy, image=pngImageB64String)
         except:
             return render_template('submit.html', error=True)
